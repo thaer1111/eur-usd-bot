e00cbf0659a7432f2e3bdf08356d86a02c95939a
thaer1111 <thaer.qaadan.88@gmail.com>
2025-06-08 14:50:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import openai
 # הגדרות
 BOT_TOKEN = '7665383679:AAGa263syK8FdyOiSXHLsUtKEKzFajbZJlM'
 CHAT_ID = '1589414763'
-OPENAI_API_KEY = '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'  
+import os
+openai.api_key = os.environ.get('OPENAI_API_KEY')  
 
 app = Flask(__name__)
 last_rate = None
